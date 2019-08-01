50238a8c1eb038126cddea0bb51985bb7e352f34
Tommy Sadiq Hinrichsen <th@mustache.dk>
2019-08-01 15:59:54 +0200

    Made SnappingCollectionViewFlowLayout public

--- MustacheUIKit/Classes/Classes/CollectionView/SnappingCollectionViewFlowLayout.swift
+++ MustacheUIKit/Classes/Classes/CollectionView/SnappingCollectionViewFlowLayout.swift
@@ -1,7 +1,7 @@
 import Foundation
 import UIKit
 
-class SnappingCollectionViewFlowLayout: UICollectionViewFlowLayout {
+public class SnappingCollectionViewFlowLayout: UICollectionViewFlowLayout {
 
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
 
