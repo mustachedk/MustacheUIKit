ada12f8c28691afce7a06f3ed8c0f08466523e40
Tommy Sadiq Hinrichsen <th@mustache.dk>
2020-09-10 09:05:23 +0200

    Not working with animations

--- Sources/MustacheUIKit/Classes/Classes/View/GradientMultipleView.swift
+++ Sources/MustacheUIKit/Classes/Classes/View/GradientMultipleView.swift
@@ -4,30 +4,31 @@ import UIKit
 
 open class GradientMultipleView: UIView {
 
-    open var startPoint: CGPoint =  .init(x: 0, y: 0) {
-        didSet { self.setNeedsLayout() }
+    open var startPoint: CGPoint = .init(x: 0, y: 0) {
+        didSet {
+            (self.layer as! CAGradientLayer).startPoint = self.startPoint
+        }
     }
 
-    open var endPoint: CGPoint =  .init(x: 0, y: 1) {
-        didSet { self.setNeedsLayout() }
+    open var endPoint: CGPoint = .init(x: 0, y: 1) {
+        didSet {
+            (self.layer as! CAGradientLayer).endPoint = self.endPoint
+        }
     }
 
     open var colors: [UIColor] = [.clear, .clear] {
-        didSet { self.setNeedsLayout() }
+        didSet {
+            (self.layer as! CAGradientLayer).colors = self.colors.map(\.cgColor)
+        }
     }
 
     open var locations: [Double] = [0, 1] {
-        didSet { self.setNeedsLayout() }
+        didSet {
+            (self.layer as! CAGradientLayer).locations = self.locations.map(\.number)
+        }
     }
 
     override class open var layerClass: AnyClass { return CAGradientLayer.self }
 
-    override open func layoutSubviews() {
-        super.layoutSubviews()
-        self.backgroundColor = .clear
-        (self.layer as! CAGradientLayer).startPoint = self.startPoint
-        (self.layer as! CAGradientLayer).endPoint = self.endPoint
-        (self.layer as! CAGradientLayer).colors = self.colors.map(\.cgColor)
-        (self.layer as! CAGradientLayer).locations = self.locations.map(\.number)
-    }
+
 }
