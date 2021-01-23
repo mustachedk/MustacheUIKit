591e7d378593615d5dadec89e2043ad9bac62dbd
Tommy Sadiq Hinrichsen <tommy.lynge@gmail.com>
2021-01-23 13:27:40 +0100

    Expanding tableview with headers

--- Sources/MustacheUIKit/Classes/Classes/TableView/ExpandingTableView.swift
+++ Sources/MustacheUIKit/Classes/Classes/TableView/ExpandingTableView.swift
@@ -8,12 +8,16 @@ open class ExpandingTableView: UITableView {
 
         guard let heightConstraint = self.heightConstraint, heightConstraint.priority.rawValue < 750  else { return }
 
-        var height: CGFloat = 0.0
         for section in 0..<(self.dataSource?.numberOfSections?(in: self) ?? 1) {
+
+            let headerHeight = (self.delegate?.tableView?(self, heightForHeaderInSection: section)) ?? self.sectionHeaderHeight
+            height += headerHeight
+
             for row in 0..<(self.dataSource?.tableView(self, numberOfRowsInSection: section) ?? 0) {
                 let rowHeight = (self.delegate?.tableView?(self, heightForRowAt: IndexPath(row: row, section: section))) ?? self.rowHeight
                 height += rowHeight
             }
+
         }
 
         heightConstraint.constant = height + self.contentInset.top + self.contentInset.bottom
@@ -26,4 +30,5 @@ open class ExpandingTableView: UITableView {
       super.layoutSubviews()
       self.isScrollEnabled = (self.contentSize.height > self.frame.height)
     }
+    
 }
