c8fcf25097f26b74022592ffddcf80a1898b1d5d
Tommy Sadiq Hinrichsen <th@mustache.dk>
2021-03-23 09:54:12 +0100

    Added sliding transition for tabbar controller Added radial support for gradient view

--- Sources/MustacheUIKit/Classes/Classes/View/GradientView.swift
+++ Sources/MustacheUIKit/Classes/Classes/View/GradientView.swift
@@ -3,6 +3,10 @@ import UIKit
 @IBDesignable
 open class GradientView: UIView {
 
+    @IBInspectable open var type: UInt = 0 {
+        didSet { self.setNeedsLayout() }
+    }
+
     @IBInspectable open var startColor: UIColor = UIColor.white {
         didSet { self.setNeedsLayout() }
     }
@@ -28,5 +32,29 @@ open class GradientView: UIView {
         (layer as! CAGradientLayer).colors = [startColor.cgColor, endColor.cgColor]
         (layer as! CAGradientLayer).startPoint = startPoint
         (layer as! CAGradientLayer).endPoint = endPoint
+        (layer as! CAGradientLayer).type = GradientViewType(rawValue: self.type)?.layerType ?? .axial
+    }
+}
+
+enum GradientViewType: UInt {
+
+    case axial = 0
+    case radial = 1
+    @available(iOS 12.0, *) case conic = 2
+
+    var layerType: CAGradientLayerType {
+        switch self {
+            case .axial:
+                return .axial
+            case .radial:
+                return .radial
+            case .conic:
+                if #available(iOS 12, *) {
+                    return .conic
+                } else {
+                    return .radial
+                }
+
+        }
     }
 }
